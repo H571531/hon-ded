0728614a86017e2d81aa285ebc74dd62b154aae4
Adrian <31210153+H571531@users.noreply.github.com>
2022-04-28 22:00:03 +0200

    Endret decimals for økt spenning 240v

--- hon-ded.py
+++ hon-ded.py
@@ -36,7 +36,7 @@ def main():
         time.sleep(1)
         
 
-def printProgressBar(iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+def printProgressBar(iteration, total, prefix = '', suffix = '', decimals = 2, length = 100, fill = '█', printEnd = "\r"):
     percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
     filledLength = int(length * iteration // total)
     bar = fill * filledLength + '-' * (length - filledLength)
